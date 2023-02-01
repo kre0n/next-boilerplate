1bdf34f59ab7f4aa20e0daed43dc4078b49ab9bf
vl.matveev <vl.matveev@corp.mail.ru>
2023-02-01 20:26:47 +0300

    cleanup

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,12 +1,8 @@
 import {PropsWithChildren} from 'react';
 
-interface ButtonProps {
-
-}
-
 export function Button({
     children
-}: PropsWithChildren<ButtonProps>) {
+}: PropsWithChildren) {
     return <button>
         {children}
     </button>
